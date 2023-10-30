0528cffaef881ce26a4156fd2433fda5bb772940
Achyuth Sosale <achyuthyogesh0@gmail.com>
2023-10-30 10:09:00 +0530

    Connect db

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 const app=express()
 async function main(){
     try{
-        await mongoose.connect('');
-        console.log("Connected!");
+        await mongoose.connect('mongodb+srv://admin:12345@cluster0.p4vtm2q.mongodb.net/?retryWrites=true&w=majority');
+        console.log("Connected!")
     }catch(error){
         console.log(error)
+        console.log("Oh no itsan error")
     }
 }
-main();
+main()
+
+/* const seedblog = async () => {
+        const blog = new Blog({
+            title: "Pes sucks",
+            author: "Aditiyaa Naag",
+            body: "Bro wtf is this college da some louda bs."
+        })
+        await blog.save();
+}
+seedblog().then(() =>{
+mongoose.connection.close()
+}); */
--- backend/models/blog.js
+++ backend/models/blog.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const blogSchema = new Schema ({
-    title: string,
-    author: string,
-    body: string
+    title: String,
+    author: String,
+    body: String
 })
 
 module.exports = mongoose.model("Blog",blogSchema)
